ca0f0b069cffef56c06e1721d7bc2016c7e4aa87
Tobias Brandy <tbrandy@itba.edu.ar>
2023-01-01 12:03:51 -0300

    internal/math.go: missing functions from stdlib

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module tobiasbrandy.com/aoc/2022
 
 go 1.19
 
-require github.com/gammazero/deque v0.2.1
+require (
+	github.com/gammazero/deque v0.2.1
+	golang.org/x/exp v0.0.0-20221230185412-738e83a70c30
+)
---
+++ internal/math.go
@@ -0,0 +1,21 @@
+package internal
+
+import "golang.org/x/exp/constraints"
+
+func IntAbs[T constraints.Signed](x T) T {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
+
+func Sign[T constraints.Signed | constraints.Unsigned | constraints.Float](x T) int {
+	switch {
+	case x > 0:
+		return 1
+	case x < 0:
+		return -1
+	default:
+		return 0
+	}
+}
