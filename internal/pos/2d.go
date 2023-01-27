0a501f6f5b239745c86809655e8ab443cadc7a6e
Tobias Brandy <tbrandy@itba.edu.ar>
2023-01-27 19:44:58 -0300

    pos.2d#Neighbours8

--- internal/pos/2d.go
+++ internal/pos/2d.go
@@ -17,7 +17,7 @@ func New2D(x, y int) D2 {
 }
 
 func (p D2) Neighbours4() [4]D2 {
-	return [4]D2{
+	return [...]D2{
 		{p.X - 1, p.Y},
 		{p.X + 1, p.Y},
 		{p.X, p.Y - 1},
@@ -25,8 +25,24 @@ func (p D2) Neighbours4() [4]D2 {
 	}
 }
 
+func (p D2) Neighbours8() [8]D2 {
+	return [...]D2{
+		{p.X - 1, p.Y - 1},
+		{p.X - 1, p.Y},
+		{p.X - 1, p.Y + 1},
+
+		{p.X, p.Y - 1},
+		// {p.X, p.Y}, Me
+		{p.X, p.Y + 1},
+
+		{p.X + 1, p.Y - 1},
+		{p.X + 1, p.Y},
+		{p.X + 1, p.Y + 1},
+	}
+}
+
 func (p D2) Distance1(o D2) int {
-	return mathext.IntAbs(p.X - o.X) + mathext.IntAbs(p.Y - o.Y)
+	return mathext.IntAbs(p.X-o.X) + mathext.IntAbs(p.Y-o.Y)
 }
 
 func (p D2) String() string {
